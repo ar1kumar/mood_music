a12137a217347b9a5b0fd06419f52519537f462e
Ar1k <arunkmr492@gmail.com>
2015-04-16 19:35:49 +0530

    minor update

--- lib/get_vid.php
+++ lib/get_vid.php
@@ -1,7 +1,8 @@
 <?php
-	$mood = $_POST['mood'];
+	//$mood = $_POST['mood'];
+	$mood = 'happy';
 	//$response = http_get("https://www.googleapis.com/youtube/v3/search?key=AIzaSyBfGJ7nuAra9imWqN8q3UsHTWyiKvGLTdU&channelId=UC-9-kyTW8ZkZNDHQJ6FgpwQ&part=snippet,id&order=date&maxResults=50", array("timeout"=>1), $info);
-	$request = file_get_contents("https://www.googleapis.com/youtube/v3/search?part=snippet&q=".$mood." song music&type=video&key=AIzaSyBfGJ7nuAra9imWqN8q3UsHTWyiKvGLTdU&maxResults=50");
+	$request = file_get_contents("https://www.googleapis.com/youtube/v3/search?part=snippet&q=".$mood."%20music&type=video&videoCaption=any&key=AIzaSyBfGJ7nuAra9imWqN8q3UsHTWyiKvGLTdU&maxResults=50");
 	//print_r($response);
 	
 	$data_back = json_decode($request);
